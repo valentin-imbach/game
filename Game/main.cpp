696d5dfb26ddd1af47011b2c730c969424ce88eb
Valentin Imbach <valentinimbach@MacVal.local>
2019-09-19 21:42:34 +0200

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Valentin Imbach on 2019-09-19.
+//  Copyright © 2019 Valentin Imbach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
