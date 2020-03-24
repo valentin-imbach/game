28bd4dcfaddfafd16eb0b531d6fc94f7a24e838d
Valentin Imbach <valentin.imbach@gmail.com>
2020-03-24 15:17:26 +0100

    test commit

---
+++ Game/Game.cpp
@@ -0,0 +1,37 @@
+//
+//  Game.cpp
+//  Game
+//
+//  Created by Valentin Imbach on 2019-09-20.
+//  Copyright © 2019 Valentin Imbach. All rights reserved.
+//
+
+#include "Game.hpp"
+#include "GameObject.hpp"
+
+Game::Game() {
+    Tile::loadTypes();
+    world = new Map(50,50);
+    player = new Player(3,3,3);
+}
+
+Game::~Game() {
+    delete world;
+    delete player;
+}
+
+void Game::update() {
+    world -> update();
+    player -> update(world);
+    Camera::update();
+}
+
+void Game::render() {
+    SDL_SetRenderDrawColor(Window::renderer, 255, 255, 255, 255);
+    SDL_RenderClear(Window::renderer);
+    world -> render();
+    player -> render();
+    player -> renderHotbar(3);
+    TextManager::drawText(std::to_string(Window::FPS).c_str(), 20, 10);
+    SDL_RenderPresent(Window::renderer);
+}
