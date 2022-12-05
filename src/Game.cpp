e821cb21a3bac141164171ce850eb260e502e6f9
Valentin Imbach <valentin.imbach@gmail.com>
2022-12-05 20:31:01 +0100

    test

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,53 +1,53 @@
 
 #include "Game.hpp"
-#include "utils/utils.hpp"
-#include "TextureManager.hpp"
 #include "TextManager.hpp"
+#include "TextureManager.hpp"
 #include "Window.hpp"
+#include "utils/utils.hpp"
 
 Game::Game() {
-    running = true;
-    lastFrameTicks = 0;
-    lastSecondTicks = 0;
-    frameCounter = 0;
-    framesPerSecond = 0;
-    TextureManager::Init();
-    TextManager::Init();
-    world = std::make_unique<World>("World");
+  running = true;
+  lastFrameTicks = 0;
+  lastSecondTicks = 0;
+  frameCounter = 0;
+  framesPerSecond = 0;
+  TextureManager::Init();
+  TextManager::Init();
+  world = std::make_unique<World>("World");
 }
 
 void Game::update() {
-    Window::instance -> clear();
-    world -> update();
-    std::string text = "FPS: " + std::to_string(framesPerSecond);
-    TextManager::drawText(text, {20,20});
-    Window::instance -> update();
+  Window::instance->clear();
+  world->update();
+  std::string text = "FPS: " + std::to_string(framesPerSecond);
+  TextManager::drawText(text, {20, 20});
+  Window::instance->update();
 }
 
 void Game::handleEvents() {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            LOG("Window closed");
-            running = false;
-            break;
-        }
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    if (event.type == SDL_QUIT) {
+      LOG("Window closed");
+      running = false;
+      break;
     }
+  }
 }
 
 void Game::limitFrameRate(int fps) {
-    uint delay = ceil(1000.0f / fps);
-    uint ticks = SDL_GetTicks();
-    uint past = ticks - lastFrameTicks;
-    if (past < delay) {
-        SDL_Delay(delay - past);
-        past = delay;
-    }
-    lastFrameTicks = ticks;
-    frameCounter += 1;
-    if (ticks - lastSecondTicks > 1000) {
-        framesPerSecond = frameCounter;
-        frameCounter = 0;
-        lastSecondTicks = ticks;
-    }
+  uint delay = ceil(1000.0f / fps);
+  uint ticks = SDL_GetTicks();
+  uint past = ticks - lastFrameTicks;
+  if (past < delay) {
+    SDL_Delay(delay - past);
+    past = delay;
+  }
+  lastFrameTicks = ticks;
+  frameCounter += 1;
+  if (ticks - lastSecondTicks > 1000) {
+    framesPerSecond = frameCounter;
+    frameCounter = 0;
+    lastSecondTicks = ticks;
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 
-#include "Window.hpp"
 #include "Game.hpp"
+#include "Window.hpp"
 
 int main() {
-    Window window("Title", {1024, 640});
-    Game game;
+	Window window("Title", {1024, 640});
+	Game game;
 
-    while (game.running) {
-        game.handleEvents();
-        game.update();
-        game.limitFrameRate(30);
-    }
+	while(game.running) {
+	    game.handleEvents();
+		game.update();
+		game.limitFrameRate(30);
+	}
 
-    return 0;
+	return 0;
 }
